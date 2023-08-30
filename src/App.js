3291684ac99b865b03bdba2917d2cf6787ac9230
kmungai <mungaikelvinw@gmail.com>
2023-08-30 03:32:34 +0300

    Added the date logic

--- src/App.js
+++ src/App.js
@@ -6,17 +6,56 @@ export default function App() {
 }
 
 function Body() {
-  const [step, setStep] = useState(1);
-  const [Date, setDate] = useState(1);
-
-  const initialDate = date.toLocaleDateString();
+  const [step, setStep] = useState(0);
+  const [date, setDate] = useState();
 
   function HandleStepPlus() {
     setStep((step) => step + 1);
+    setDate((date) => date.myDate() + step);
   }
 
   function HandleStepMinus() {
     return step > 1 ? setStep((step) => step - 1) : null;
+  }
+
+  // function HandleStepMinus() {}
+
+  function myDate() {
+    var today = new Date();
+    var todayDate = today.getDate();
+    var day = today.getDay();
+    var month = today.getMonth();
+    var year = today.getFullYear();
+
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    var newMonth = months[month];
+
+    var daysOfWeek = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
+    var newDays = daysOfWeek[day];
+
+    return `${newDays} ${newMonth} ${todayDate} ${year}`;
   }
 
   return (
@@ -37,9 +76,7 @@ function Body() {
         <button>+</button>
       </div>
 
-      <p className="dateBar"> {}</p>
+      <p className="dateMessage">{date}</p>
     </div>
   );
 }
-
-function HandleStepMinus() {}
